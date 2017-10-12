25dcb34eb86da8d3708155732ed25c7cf494163c
Bill Dueber <bill@dueber.com>
2017-10-12 11:51:36 -0400

    Move stuff

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,70 +8,6 @@ class Bottles
     upper.downto(lower).map {|i| verse(i)}.join("\n")
   end
 
-  class BottleSet
-    attr_reader :num
-
-    def initialize(n)
-      @num = n
-    end
-
-    def description
-      "#{num} bottles"
-    end
-
-    def pronoun
-      "one"
-    end
-
-    def next
-      self.class.from(num - 1)
-    end
-
-    def instruction
-      "Take #{pronoun} down and pass it around"
-    end
-
-    def self.from(num)
-      case num
-      when 0
-        NoBottles.new(num)
-      when 1
-        SingleBottle.new(num)
-      else
-        self.new(num)
-      end
-    end
-  end
-
-  class SingleBottle < BottleSet
-    def pronoun
-      "it"
-    end
-
-    def description
-      "1 bottle"
-    end
-
-    def next
-      BottleSet.from(0)
-    end
-  end
-
-  class NoBottles < BottleSet
-    def description
-      "no more bottles"
-    end
-
-    def instruction
-      "Go to the store and buy some more"
-    end
-
-    def next
-      BottleSet.from(99)
-    end
-  end
-
-
   def verse(number)
     bs = BottleSet.from(number)
     "#{bs.description.capitalize} of beer on the wall, #{bs.description} of beer.\n" +
@@ -79,3 +15,85 @@ class Bottles
   end
 
 end
+
+
+class BottleSet
+  attr_reader :num
+
+  def initialize(n)
+    @num = n
+  end
+
+  def description
+    "#{num} bottles"
+  end
+
+  def pronoun
+    "one"
+  end
+
+  def next
+    self.class.from(num - 1)
+  end
+
+  def instruction
+    "Take #{pronoun} down and pass it around"
+  end
+
+  def self.from(num)
+    case num
+    when 0
+      NoBottles.new(num)
+    when 1
+      SingleBottle.new(num)
+    when 6
+      SixPack.new(num)
+    else
+      self.new(num)
+    end
+  end
+end
+
+class SingleBottle < BottleSet
+  def pronoun
+    "it"
+  end
+
+  def description
+    "1 bottle"
+  end
+
+  def next
+    BottleSet.from(0)
+  end
+end
+
+class NoBottles < BottleSet
+  def description
+    "no more bottles"
+  end
+
+  def instruction
+    "Go to the store and buy some more"
+  end
+
+  def next
+    BottleSet.from(99)
+  end
+end
+
+class SixPack < BottleSet
+  def description
+    "1 six pack"
+  end
+
+  def pronoun
+    "it"
+  end
+
+  def next
+    BottleSet.from(0)
+  end
+
+end
+
