5a897cf296f741abfe819eed8090795f32d0162c
Aaron Elkiss <aelkiss@umich.edu>
2017-10-12 11:24:44 -0400

    walk earleyj through flocking rules

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,3 +1,58 @@
+class BottleNumber
+
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def action()
+    case number
+    when 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun} down and pass it around"
+    end
+  end
+
+  def successor()
+    case number
+    when 0
+      99
+    else
+      number - 1
+    end
+  end
+
+  def quantity()
+    case number
+    when 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def pronoun()
+    case number
+    when 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def container()
+    case number
+    when 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+end
+
 class Bottles
 
   def song
@@ -9,16 +64,30 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-    else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
-    end
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+        "#{quantity(number)} #{container(number)} of beer.\n" +
+        "#{action(number)}, " +
+        "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+  end
+
+  def action(number)
+    BottleNumber.new(number).action()
+  end
+
+  def successor(number)
+    BottleNumber.new(number).successor()
+  end
+
+  def quantity(number)
+    BottleNumber.new(number).quantity()
+  end
+
+  def pronoun(number)
+    BottleNumber.new(number).pronoun()
+  end
+
+  def container(number)
+    BottleNumber.new(number).container()
   end
 
 end
