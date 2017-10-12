80b64dbcada5b1208c0303665d893ad0f4d0bf1f
Bill Dueber <bill@dueber.com>
2017-10-12 11:35:12 -0400

    Commit

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -5,20 +5,104 @@ class Bottles
   end
 
   def verses(upper, lower)
-    upper.downto(lower).map { |i| verse(i) }.join("\n")
+    upper.downto(lower).map {|i| verse(i)}.join("\n")
   end
 
-  def verse(number)
-    case number
+  def beer_bottle_group(num)
+    case num
     when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+      "no more bottles"
     when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+      "1 bottle"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+      "#{num} bottles"
     end
+  end
+
+  def pronoun(num)
+    case num
+    when 1
+      'it'
+    else
+      'one'
+    end
+  end
+
+  class BottleSet
+    attr_reader :num
+
+    def initialize(n)
+      @num = n
+    end
+
+    def description
+      case num
+      when 0
+        "no more bottles"
+      when 1
+        "1 bottle"
+      else
+        "#{num} bottles"
+      end
+    end
+
+    def pronoun
+      "one"
+    end
+
+    def prev
+      self.class.from(num - 1)
+    end
+
+    def instruction
+      "Take #{pronoun} down and pass it around"
+    end
+
+    def self.from(num)
+      case num
+      when 0
+        NoBottles.new(num)
+      when 1
+        SingleBottle.new(num)
+      else
+        self.new(num)
+      end
+    end
+  end
+
+  class SingleBottle < BottleSet
+    def pronoun
+      "it"
+    end
+
+    def description
+      "1 bottle"
+    end
+
+    def prev
+      BottleSet.from(0)
+    end
+  end
+
+  class NoBottles < BottleSet
+    def pronoun
+      "no more"
+    end
+
+    def instruction
+      "Go to the store and buy some more"
+    end
+
+    def prev
+      BottleSet.from(99)
+    end
+  end
+
+
+  def verse(number)
+    bs = BottleSet.from(number)
+    "#{bs.description.capitalize} of beer on the wall, #{bs.description} of beer.\n" +
+        "#{bs.instruction}, #{bs.prev.description} of beer on the wall.\n"
   end
 
 end
